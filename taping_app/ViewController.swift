849ea1e164eccc581c645d9674ae88483e7f23ee
清水みさき <ankiku.ms@gmail.com>
2018-09-01 12:48:31 +0900

    Initial Commit

---
+++ taping_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taping_app
+//
+//  Created by 清水みさき on 2018/09/01.
+//  Copyright © 2018年 清水みさき. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
